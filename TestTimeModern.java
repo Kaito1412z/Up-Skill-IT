fbd9193844b8caf264cbeed94ee7b3ee525cd413
Kaito1412z <131657707+Kaito1412z@users.noreply.github.com>
2025-08-02 16:03:55 +0700

    Time Modern

---
+++ TestTimeModern.java
@@ -0,0 +1,19 @@
+public class TestTimeModern {
+    public static void main(String[] args) {
+        TimeModern t = new TimeModern(23, 59, 59);
+        System.err.println("Start time: "+t);
+
+        TimeModern t2 = t.nextSecond();
+        System.err.println("After 1 second: "+t2);
+
+        TimeModern t3 = t2.nextSecond().nextSecond();
+        System.err.println("After 2 more seconds: "+t3);
+
+        System.out.println("Hour: "+ t3.getHour());
+        System.out.println("Minute: "+ t3.getMinute());
+        System.out.println("Second: "+ t3.getSecond());
+
+        TimeModern t4 = t3.withTime(7, 30, 15);
+        System.out.println("New time with withtime() "+ t4);
+    }
+}
